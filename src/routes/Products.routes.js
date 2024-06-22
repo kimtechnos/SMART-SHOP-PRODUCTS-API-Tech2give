72fbe80d64ea9f4b423ce2312e3867e5ff52dea7
kimtechnos <francis>
2024-06-22 15:24:07 +0300

    implement Get a single product route

--- src/routes/Products.routes.js
+++ src/routes/Products.routes.js
@@ -11,8 +11,20 @@ router.get("/", async (req, res) => {
     res.status(500).json({ success: false, message: err.message });
   }
 });
-router.get("/:id", (req, res) => {
-  res.send("getting a single product");
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const result = await pool.query("SELECT * FROM products WHERE id=$1", [id]);
+    if (result.rowCount === 0) {
+      res.status(404).json({ success: false, message: "product not found" });
+    } else {
+      res.status(200).json({ success: true, data: result.rows[0] });
+    }
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+  //   res.send("getting a single product");
 });
 router.post("/", (req, res) => {
   res.send("creating a product");
