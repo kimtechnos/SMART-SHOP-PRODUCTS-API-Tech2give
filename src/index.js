dd9cd820d29ae0e9a045431895e4a69204675ef7
kimtechnos <francis>
2024-06-22 20:10:44 +0300

    implement add/post product route

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import productsRouter from "./routes/Products.routes.js";
 
 const app = express();
+app.use(express.json());
 app.use("/products", productsRouter);
-app.listen(3000, () => {
-  console.log("server running on port 3000");
+app.listen(3001, () => {
+  console.log("server running on port 3001");
 });
--- src/routes/Products.routes.js
+++ src/routes/Products.routes.js
@@ -24,14 +24,47 @@ router.get("/:id", async (req, res) => {
   } catch (err) {
     res.status(500).json({ success: false, message: err.message });
   }
-  //   res.send("getting a single product");
 });
-router.post("/", (req, res) => {
-  res.send("creating a product");
+const validateProductInformation = (req, re, next) => {
+  const productThumbnail = req.body.productThumbnail;
+  const productTitle = req.body.productTitle;
+  const productDescription = req.body.productDescription;
+  const productCost = req.body.productCost;
+  const onOffer = req.body.onOffer;
+  next();
+};
+router.post("/", validateProductInformation, async (req, res) => {
+  try {
+    // if(!productTitle) return res.status(400).json({success:false,message:"product title required"});
+    const productThumbnail = req.body.productThumbnail;
+    const productTitle = req.body.productTitle;
+    const productDescription = req.body.productDescription;
+    const productCost = req.body.productCost;
+    const onOffer = req.body.onOffer;
+    const insert = await pool.query(
+      "INSERT INTO products(productThumbnail, productTitle, productDescription, productCost, onOffer) VALUES($1, $2, $3, $4, $5)",
+      [
+        productThumbnail,
+        productTitle,
+        productDescription,
+        productCost,
+        onOffer,
+      ],
+    );
+    if (insert.rowCount === 1) {
+      res
+        .status(201)
+        .json({ success: true, message: "product created successfully" });
+    }
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
 });
+
 router.patch("/:id", (req, res) => {
   res.send("updating a product");
 });
+
 router.delete("/:id", (req, res) => {
   res.send("deleting a product");
 });
