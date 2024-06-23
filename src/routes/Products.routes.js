384c6bf355bddc8dcd2516fda63a1056ab4717af
kimtechnos <francis>
2024-06-23 10:41:13 +0300

    update the patch router

--- src/routes/Products.routes.js
+++ src/routes/Products.routes.js
@@ -61,8 +61,58 @@ router.post("/", validateProductInformation, async (req, res) => {
   }
 });
 
-router.patch("/:id", (req, res) => {
-  res.send("updating a product");
+router.patch("/:id", async (req, res) => {
+  const id = req.params.id;
+  const {
+    productThumbnail,
+    productTitle,
+    productDescription,
+    productCost,
+    onOffer,
+  } = req.body;
+  try {
+    let updateOperation;
+    if (productTitle) {
+      updateOperation = await pool.query(
+        "UPDATE products SET productTitle=$1 WHERE id=$2",
+        [productTitle, id],
+      );
+    }
+    if (productThumbnail) {
+      updateOperation = await pool.query(
+        "UPDATE products SET productThumbnail=$1 WHERE id=$2",
+        [productThumbnail, id],
+      );
+    }
+    if (productDescription) {
+      updateOperation = await pool.query(
+        "UPDATE products SET productDescription=$1 WHERE id=$2",
+        [productDescription, id],
+      );
+    }
+    if (productCost) {
+      updateOperation = await pool.query(
+        "UPDATE products SET productCost=$1 WHERE id=$2",
+        [productCost, id],
+      );
+    }
+    if (onOffer) {
+      updateOperation = await pool.query(
+        "UPDATE products SET onOffer=$1 WHERE id=$2",
+        [onOffer, id],
+      );
+    }
+    if (updateOperation.rowCount === 1) {
+      res
+        .status(200)
+        .json({ success: true, message: "product updated successfully" });
+    } else {
+      res.status(400).json({ success: false, message: "Invalid product" });
+    }
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+  //  res.send("updating a product");
 });
 
 router.delete("/:id", async (req, res) => {
